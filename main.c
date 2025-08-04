da2be43b6e278388561646abade7ce4ad51a208e
zhuzhaob <zhuzhaob@stanford.edu>
2025-08-04 16:48:48 -0700

    8/14 pseudo

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 // psuedocode 
-fd = socket()
-bind(fd, address)
-listen(fd)
-while True:
-    conn_fd = accept(fd)
-    do_something_with(conn_fd)
-    close(conn_fd)
+// fd = socket()
+// bind(fd, address)
+// listen(fd)
+// while True:
+//     conn_fd = accept(fd)
+//     do_something_with(conn_fd)
+//     close(conn_fd)

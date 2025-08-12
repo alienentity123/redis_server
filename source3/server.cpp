98521d1808da718ab96828687880d43660f29de8
zhuzhaob <zhuzhaob@stanford.edu>
2025-08-12 14:34:43 -0700

    8/12 server

--- source3/server.cpp
+++ source3/server.cpp
@@ -33,5 +33,42 @@ static void do_something(int connfd){
 
 int main(){
     int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        die("socket()");
+    }
 
+    // this is needed for most server applications
+    int val = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+    // bind
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(0);    // wildcard address 0.0.0.0
+    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv) {
+        die("bind()");
+    }
+
+    // listen
+    rv = listen(fd, SOMAXCONN);
+    if (rv) {
+        die("listen()");
+    }
+
+    while (true) {
+        // accept
+        struct sockaddr_in client_addr = {};
+        socklen_t addrlen = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
+        if (connfd < 0) {
+            continue;   // error
+        }
+
+        do_something(connfd);
+        close(connfd);
+    }
+
+    return 0;
 }

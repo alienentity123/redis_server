609ad481eeb646149496f1fa2c01f48f06b94bb1
zhuzhaob <zhuzhaob@stanford.edu>
2025-08-13 15:18:54 -0700

    8/13 c&s bugs

--- source3/client.cpp
+++ source3/client.cpp
@@ -19,7 +19,20 @@ static void die(const char *msg){
 }
 
 static int32_t read_full(int fd, char *buf, size_t n){
-    while (n>0){
+    while (n > 0){
+        size_t rv = read(fd, buf, n);
+        if (rv <= 0){
+            return -1; //error or expectedEOF
+        }
+        assert((size_t)rv <= n);
+        n-= (size_t)rv;
+        buf += rv;
+    }
+    return 0; //success 
+}
+
+static int32_t write_all(int fd, char *buf, size_t n){
+    while (n > 0){
         size_t rv = read(fd, buf, n);
         if (rv <= 0){
             return -1; //error or expectedEOF
